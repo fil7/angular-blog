a4aca7cab3c2ab9c1485fed49a718c3e2dbe731d
filonova_ma <filonova7@yandex.ru>
2020-05-03 16:14:31 +0700

    Created Admin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,21 +11,12 @@ const routes: AppRoute[] = [
     path: '',
     component: MainLayoutComponent,
     children: [
-      {
-        path: '',
-        redirectTo: '/',
-        pathMatch: 'full'
-      },
-      {
-        path: '',
-        component: HomePageComponent
-      },
-      {
-        path: 'post/:id',
-        component: PostPageComponent
-      }
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: HomePageComponent },
+      { path: 'post/:id', component: PostPageComponent }
     ]
-  }
+  },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
 ];
 
 @NgModule({
