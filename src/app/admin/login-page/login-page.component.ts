2eb5db80a363a2af6966e2baffafb4d00882d254
filonova_ma <filonova7@yandex.ru>
2020-05-21 01:24:46 +0700

    Created login page

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: [ './login-page.component.scss' ]
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  get emailControl(): AbstractControl | null {
+    return this.form.get('email');
+  }
+
+  get passwordControl(): AbstractControl | null {
+    return this.form.get('password');
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [ Validators.email, Validators.required ]),
+      password: new FormControl(null, [ Validators.required, Validators.minLength(6) ])
+    });
+  }
+
+  submit(): void {
+
+    if (this.form.valid) {
+      const { email, password } = this.form.value;
+      const user: User = { email, password };
+    }
+  }
+
+  isInvalidControl(field: 'email' | 'password'): boolean {
+    const control = this.form.get(field);
+    return !!control && control.touched && control.invalid;
+  }
 }
