4c1c4d7a9e67256777d42e6da9a9bee2cca780ad
filonova_ma <filonova7@yandex.ru>
2020-05-25 19:10:42 +0700

    Set up Firebase

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -2,3 +2,12 @@ export interface User {
   email: string;
   password: string;
 }
+
+export interface FbAuthResponse {
+  idToken: string;
+  displayName: string;
+  email: string;
+  kind: string;
+  localId: string;
+  registered: boolean;
+}
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../interfaces';
+import { FbAuthResponse, User } from '../interfaces';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
   get token(): string {
     return '';
   }
+
   // private _token: string;
 
-  constructor(private http: HttpClient  ) {
+  constructor(private http: HttpClient) {
 
   }
 
-  login(user: User): Observable<User> {
-    return this.http.post('', user) as Observable<User>;
+  login(user: User): Observable<any> {
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      );
   }
 
   logout() {
@@ -26,7 +32,7 @@ export class AuthService {
     return !!this.token;
   }
 
-  private setToken(value: string) {
-
+  private setToken(response: any) {
+    console.log(response);
   }
 }
